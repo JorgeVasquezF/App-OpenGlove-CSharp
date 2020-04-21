6785966602caab71bf7fcd700bd436e5b294b1ca
jorge vasquez <jorge.vasquez.f@usach.cl>
2020-04-21 03:17:50 -0400

    API CSharp

---
+++ Assets/Glove/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform theDest;
+    public string impact = "255";
+    public UnityHapticGlove Uglove;
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+        Uglove.ActivateMotorRing(impact);
+        Uglove.ActivateMotorIndex(impact);
+        Uglove.ActivateMotorMiddle(impact);
+        Uglove.ActivateMotorPinky(impact);
+        Uglove.ActivateMotorThumb(impact);
+
+    }
+    
+    void OnMouseUp()
+    {
+        Uglove.DeactivateMotorRing();
+        Uglove.DeactivateMotorIndex();
+        Uglove.DeactivateMotorMiddle();
+        Uglove.DeactivateMotorPinky();
+        Uglove.DeactivateMotorThumb();
+
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        
+    }
+}
